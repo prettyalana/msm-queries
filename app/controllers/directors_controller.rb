2fca5dd75a5445edcd326909943b14829efe17cf
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-02-10 22:42:00 +0000

    Adds directors#show

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,13 @@ class DirectorsController < ApplicationController
   def index
     render({:template => "director_templates/list"})
   end
+  def show
+    directors_details = params.fetch("the_id")
+
+    matching_records = Director.where({:id => directors_details})
+
+    @the_director = matching_records[0]
+
+    render({:template => "director_templates/details"})
+  end
 end
